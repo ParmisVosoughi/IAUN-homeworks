31bccb87a995a2759fd877b0f0a84bc3d1f49018
Parmis <159687328+parmisvosoghi1235@users.noreply.github.com>
2025-10-28 18:54:07 +0000

    COMMIT

---
+++ Age-to-time.py
@@ -0,0 +1,10 @@
+age = int(input("سن خود را وارد کنید: "))
+
+days = age * 365
+hours = days * 24
+minutes = hours * 60
+seconds = minutes * 60
+
+print(f"ثانیه: {seconds}")
+print(f"دقیقه: {minutes}")
+print(f"ساعت: {hours}")
